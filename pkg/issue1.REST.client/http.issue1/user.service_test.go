1e276c33e041a03396f8a67e700970c3a5422505
slim-crown <michaelzewdu10@gmail.com>
2020-01-27 21:27:14 +0300

    Started testing on the get user handler

--- pkg/issue1.REST.client/http.issue1/user.service_test.go
+++ pkg/issue1.REST.client/http.issue1/user.service_test.go
@@ -20,5 +20,19 @@ func TestAddUser(t *testing.T) {
 	if wantResult != gotResult.Username {
 		t.Errorf("Wrong result recevied")
 	}
+}
+
+func TestGetUser(t *testing.T) {
+	us1 := UserService{}
+	value := "slim"
+	gotResult, err := us1.GetUser(value)
+
+	if err != nil {
+		t.Errorf("GetUser() returned error %s", err)
+	}
+
+	if value != gotResult.Username {
+		t.Errorf("GetUser() method returned invalid value")
+	}
 
 }
